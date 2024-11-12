f9d6f02f3f8b3c6508ef2f244975c7972abb0a3a
Satria-WS <extencezone02@gmail.com>
2024-11-12 18:10:55 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://satria-ws.github.io/shophy-ver1/"
 })
+
+
+
+//   "homepage": "https://satria-ws.github.io/shophy-ver1/",
