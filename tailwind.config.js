34fb8115311bfc4a4f7ff4b5e81bef3d246cdbce
Satria-WS <extencezone02@gmail.com>
2024-11-12 18:10:49 +0700

    configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,27 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode:"class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fea928",
+        secondary: "#ed8900",
+      },
+      fontFamily: {
+        san:[""]
+      },
+      container: {
+        center: true,
+        padding: {
+          sm: "2rem",
+          md: "3rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        }
+      }
+    },
   },
   plugins: [],
 }
