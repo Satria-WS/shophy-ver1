c57fc581e7c54e508565e2bd440cfe2659abbcd2
Satria-WS <extencezone02@gmail.com>
2024-11-13 16:38:08 +0700

    update footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Slider from "react-slick";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+// Slick Carousel settings
+const settings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 800,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  cssEase: "ease-in-out",
+  pauseOnHover: false,
+  pauseOnFocus: true,
+};
+
+const Footer: React.FC = () => {
+  return (
+    <>
+      {/* <div>
+        <Slider {...settings}>
+          <div>
+            <h3>Slide 1</h3>
+          </div>
+          <div>
+            <h3>Slide 1</h3>
+          </div>
+        </Slider>
+      </div> */}
+    </>
+  );
+};
+
+export default Footer;
