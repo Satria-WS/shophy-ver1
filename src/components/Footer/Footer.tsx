b780185a92e203adc20e1e281db4c68f0c529ab3
Satria-WS <extencezone02@gmail.com>
2024-11-14 16:52:09 +0700

    adding the last component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,34 +3,42 @@ import Slider from "react-slick";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 // Slick Carousel settings
-const settings = {
-  dots: false,
-  arrows: false,
-  infinite: true,
-  speed: 800,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  cssEase: "ease-in-out",
-  pauseOnHover: false,
-  pauseOnFocus: true,
-};
+// const settings = {
+//   dots: false,
+//   arrows: false,
+//   infinite: true,
+//   speed: 800,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 4000,
+//   cssEase: "ease-in-out",
+//   pauseOnHover: false,
+//   pauseOnFocus: true,
+// };
 
 const Footer: React.FC = () => {
   return (
     <>
-      {/* <div>
-        <Slider {...settings}>
-          <div>
-            <h3>Slide 1</h3>
-          </div>
-          <div>
-            <h3>Slide 1</h3>
-          </div>
-        </Slider>
-      </div> */}
+      <div>Footer</div>
     </>
   );
 };
 
 export default Footer;
+
+// const Footer: React.FC = () => {
+//   return (
+//     <>
+//       {/* <div>
+//         <Slider {...settings}>
+//           <div>
+//             <h3>Slide 1</h3>
+//           </div>
+//           <div>
+//             <h3>Slide 1</h3>
+//           </div>
+//         </Slider>
+//       </div> */}
+//     </>
+//   );
+// };
