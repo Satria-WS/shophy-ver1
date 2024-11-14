2d62df5ee9a0099a3d79bc00c1cac582b2cd2ff9
Satria-WS <extencezone02@gmail.com>
2024-11-14 16:51:09 +0700

    adding data

--- src/components/Product/BestProduct.tsx
+++ src/components/Product/BestProduct.tsx
@@ -2,10 +2,41 @@ import shirt1 from "../../assets/shirt/shirt.png";
 import shirt2 from "../../assets/shirt/shirt2.png";
 import shirt3 from "../../assets/shirt/shirt3.png";
 import { FaStar } from "react-icons/fa";
+
+interface data {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
+
+const ProductsData: data[] = [
+  {
+    id: 1,
+    img: shirt1,
+    title: "Casual Wear",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    img: shirt2,
+    title: "Printed shirt",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    img: shirt3,
+    title: "Women shirt",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
 const BestProduct = () => {
   return (
     <>
-      <div className="container  p-5">
+      <div className="container  p-5 overflow-y-hidden">
         {/* text */}
         <div className="flex flex-col items-center text-center">
           <span className="text-primary">Top Best Rated Products for you</span>
@@ -15,38 +46,46 @@ const BestProduct = () => {
             ut!
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center py-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center pt-20">
           {/* card */}
-          <div className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white  shadow-xl duration-300 group max-w-[300px] aos-init aos-animate py-5">
-            {/* image */}
-            <div className="h-[140px]">
-              <img
-                className="max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"
-                src={shirt1}
-                alt="shirt-1"
-              />
-            </div>
-            {/* text-card */}
-            <div>
-              {/* star */}
-              <div>
-                <ul className="flex space-x-1">
-                  {/* Create the number of stars dynamically */}
-                  {[...Array(5)].map((_, index) => (
-                    <li key={index}>
-                      <FaStar className="text-yellow-500" />
-                    </li>
-                  ))}
-                </ul>
+          {ProductsData.map((item, index) => (
+            <>
+              <div
+                data-aos="fade-up"
+                data-aos-delay="0"
+                key={index}
+                className=" aos-init aos-animate px-3 rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white  shadow-xl duration-300 group max-w-[300px]  py-5 mb-7"
+              >
+                {/* image */}
+                <div className="h-[140px]">
+                  <img
+                    className="max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"
+                    src={item.img}
+                    alt="shirt-1"
+                  />
+                </div>
+                {/* text-card */}
+                <div className="flex flex-col justify-center items-center text-center space-y-2">
+                  {/* star */}
+                  <div>
+                    <ul className="flex space-x-1">
+                      {/* Create the number of stars dynamically */}
+                      {[...Array(5)].map((_, index) => (
+                        <li key={index}>
+                          <FaStar className="text-yellow-500" />
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <h1 className="text-xl font-bold">{item.title}</h1>
+                  <p>{item.description}</p>
+                  <button className="bg-primary hover:bg-white text-white hover:text-primary  py-2  px-4 rounded-full">
+                    <span>Order Now</span>
+                  </button>
+                </div>
               </div>
-
-              <h1>Casual Wear</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates, repudiandae?
-              </p>
-            </div>
-          </div>
+            </>
+          ))}
         </div>
       </div>
     </>
