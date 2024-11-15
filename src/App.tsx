8edbff24118cfec72b38888a2fffbb69be88d416
Satria-WS <extencezone02@gmail.com>
2024-11-15 17:50:33 +0700

    remove useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Header/Navbar";
 import Hero from "./components/Hero/Hero";
